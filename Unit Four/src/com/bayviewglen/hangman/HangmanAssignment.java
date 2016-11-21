381bd8183ad7ad57b372ef1ba118ddd63cccb7b1
dchan <dchan@dchansp3.bayviewglen.ca>
2016-11-20 22:49:14 -0500

    Still working on it.......

--- Unit Four/src/com/bayviewglen/hangman/HangmanAssignment.java
+++ Unit Four/src/com/bayviewglen/hangman/HangmanAssignment.java
@@ -9,7 +9,27 @@ public class HangmanAssignment {
 	
 	final String ALPHANUMERALS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
 	final String NO_SPACE_ALPHANUMERALS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	final int 
+	final int PLAYER_ONE_TURN = 1;
+	final int PLAYER_TWO_TURN = 0;
+	final int CHAR_OVER_ONE = 1; 
+	final int PLAYER_TURN = 2; 
+	final int POINTS = 8;
+	final int GUESS = 7;
+	final int ROUNDS = 10;
+	final int CHAR_BEGIN = 0;
+	final int INDEX_NEED = -1; 
+	final int SPACE_NUM = 30;
+	
+	String Guesses = "";
+	String playerNow = null;
+	String playerOpponent = null; 
+	String guess = null;
+	String message= null;
+	String cover = null; 
+	int playerOnePoints = 0;
+	int playerTwoPoints = 0;
+	int rounds = 1;
+	int guessed;
 	
 	
 	System.out.println("Please enter name for Player 1:");
@@ -18,8 +38,28 @@ public class HangmanAssignment {
 	System.out.println("Please enter name for Player 2:");
 	String player2 = keyboard.nextLine();
 	
-	System.out.println(player1 + ", enter your word/phrase that you want " + player2 + " to guess");
-	String wordToGuess= keyboard.nextLine().toUpperCase();
+	boolean endGame = false;
+	while (!endGame) {
+	System.out.println(" --------------- Round " + rounds + " --------------- ");
+	Guesses = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	guessed = 0;
+	cover = "";
+	
+	if (rounds % PLAYER_TURN == PLAYER_ONE_TURN){
+		playerNow = player1;
+		playerOpponent = player2;
+	} else if (rounds % PLAYER_TURN == PLAYER_TWO_TURN){
+		playerNow = player2;
+		playerOpponent = player1;
+	}
+	
+	System.out.println(playerOpponent + ", enter your word/phrase that you want " + player2 + " to guess");
+	message = keyboard.nextLine().toUpperCase();
+	}
+	
+	
+	
+	
 	
 	for (int i=0; i < wordToGuess.length(); i++){
 		if  ((int) wordToGuess.charAt(i) >= 65 && (int) wordToGuess.charAt(i) <= 90 || ((int) wordToGuess.charAt(i) >=48 || (int) wordToGuess.charAt(i) >=57)){
