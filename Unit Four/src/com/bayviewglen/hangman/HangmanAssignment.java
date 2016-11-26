5eeeb88aa98f61655097a6f1befcd0e83606b835
dchan <dchan@dchansp3.bayviewglen.ca>
2016-11-26 14:17:08 -0500

    ALMOST DONE

--- Unit Four/src/com/bayviewglen/hangman/HangmanAssignment.java
+++ Unit Four/src/com/bayviewglen/hangman/HangmanAssignment.java
@@ -20,21 +20,21 @@ public class HangmanAssignment {
 		final int INDEX_NEED = -1;
 		final int SPACE_NUM = 30;
 
-		String guesses = "";
-		String playerNow = null;
-		String playerOpponent = null;
-		String guess = null;
-		String message = null;
-		String cover = null;
+		String guesses = " ";
+		String playerNow = " ";
+		String playerOpponent = " ";
+		String guess = " ";
+		String message = " ";
+		String cover = " ";
 		int playerOnePoints = 0;
 		int playerTwoPoints = 0;
 		int rounds = 1;
 		int guessed;
 
-		System.out.println("Please enter name for Player 1:");
+		System.out.print("Please enter name for Player 1:");
 		String player1 = keyboard.nextLine().trim();
 
-		System.out.println("Please enter name for Player 2:");
+		System.out.print("Please enter name for Player 2:");
 		String player2 = keyboard.nextLine().trim();
 
 		boolean endGame = false;
@@ -52,10 +52,10 @@ public class HangmanAssignment {
 				playerOpponent = player1;
 			}
 
-			System.out.println(playerOpponent + ", enter the word/phrase you want the other player to guess: ");
-			message = keyboard.nextLine().toUpperCase();
+			System.out.print(playerOpponent + ", enter the word/phrase you want the other player to guess: ");
 			boolean crossed = true;
 			while (crossed) {
+				message = keyboard.nextLine().toUpperCase().trim();
 				crossed = false;
 				if (message.length() == 0) {
 					System.out.println("You can't do that" + playerOpponent + ", put a phrase to be guessed: ");
@@ -64,7 +64,7 @@ public class HangmanAssignment {
 
 				for (int i = 0; i < message.length(); i++) {
 					if (ALPHANUMERALS.indexOf(message.charAt(i)) == INDEX_NEED) {
-						System.out.print("Please enter an alphanumeric message: ");
+						System.out.print("Please enter an alphanumeric message!! ");
 
 						crossed = true;
 						break;
@@ -80,157 +80,153 @@ public class HangmanAssignment {
 					cover += "/";
 				else
 					cover += "_";
-				}
-			
-			for (int i = 0; i < cover.length(); i++){
+			}
+
+			for (int i = 0; i < cover.length(); i++) {
 				System.out.print(cover.charAt(i) + " ");
 			}
 			System.out.println();
 
-				boolean round = true;
-				while (guessed < GUESS && round) {
-					System.out.print(playerNow + " you used " + guessed
-							+ " guesses, do you want to now (1) solve the entire problem or (2) guess a character? ");
-					String userInput = keyboard.nextLine();
-					while (!(userInput.equals("1") || userInput.equals("2"))) {
-						System.out.println(
-								playerNow + " You have to (1) solve the entire problem or (2) guess a character! ");
-						userInput = keyboard.nextLine();
-					}
-
-					if (userInput.equals("1")) {
-						System.out.println(playerNow + ", please type your answer: ");
-						crossed = true;
-						while (crossed) {
-							guess = keyboard.nextLine().toUpperCase();
-							crossed = false;
-							for (int j = 0; j < guess.length(); j++) {
-								if (ALPHANUMERALS.indexOf(guess.charAt(j)) == INDEX_NEED) {
-									System.out
-											.print(" You can't do that, please enter an alphanumeric character only: ");
-									guess = keyboard.nextLine().toUpperCase();
-									crossed = true;
-									break;
-								}
-							}
-						}
-
-						if (guess.equals(message)) {
-							System.out.println(playerNow + ", thats right!");
-							round = false;
-							guessed--;
-
-						} else {
-							System.out.println(playerNow + ", sorry thats wrong!");
-						}
-					} else if (userInput.equals("2")) {
-						System.out.println(" Characters you didn't use yet: ");
-						for (int k = 0; k < guesses.length(); k++) {
-							System.out.print(guesses.charAt(k) + " ");
-						}
-						System.out.println( playerNow + ", please guess a single character: ");
-						crossed = true;
-						while (crossed) {
-							guess = keyboard.nextLine().toUpperCase();
-							if (NO_SPACE_ALPHANUMERALS.indexOf(guess) == INDEX_NEED) {
-								System.out.println(
-										" You can't do that, please enter a alphanumeric character, WITH NO SPACES!!");
-
-							} else if (guess.length() > 1 || guess.length() < 1) {
-								System.out.println("You can't do that, please guess a letter!!");
-							} else if (guesses.indexOf(guess) == INDEX_NEED) {
-								System.out.println(
-										" You can't do that, please guess a character YOU HAVE NOT GUESSED BEFORE!!");
-							} else {
-								crossed = false;
-								guesses = guesses.substring(CHAR_BEGIN, guess.indexOf(guess)) + " _ "
-										+ guesses.substring(guess.indexOf(guess) + CHAR_OVER_ONE);
-							}
-						}
-
-						if (message.indexOf(guess) != INDEX_NEED) {
-							System.out.println(playerNow + ", you have already guessed this character!!");
-							for (int z = 0; z < cover.length(); z++) {
-								if (message.substring(z, z + CHAR_OVER_ONE).equals(guess))
-									cover = cover.substring(CHAR_BEGIN, z) + guess + cover.substring(z + CHAR_OVER_ONE);
-
-							}
-						} else {
-							System.out.println(playerNow + " this character is not in the string yet!!");
-						}
-
-						for (int a = 0; a < cover.length(); a++) {
-							System.out.print(cover.charAt(a) + " ");
-						}
-
-						System.out.println();
-						if (cover.indexOf("_") == INDEX_NEED) {
-							System.out.println("YOU WON!!!!!!!!!!!!!");
-							guessed--;
-							round = false;
-						}
-
-					}
-
-					guessed++;
+			boolean round = true;
+			while (guessed < GUESS && round) {
+				System.out.print(playerNow + " you used " + guessed
+						+ " guesses, do you want to now (1) solve the entire problem or (2) guess a character? ");
+				String userInput = keyboard.nextLine();
+				while (!(userInput.equals("1") || userInput.equals("2"))) {
+					System.out
+							.print(playerNow + " You have to (1) solve the entire problem or (2) guess a character! ");
+					userInput = keyboard.nextLine();
 				}
-				if (guessed == GUESS) {
-					System.out.print(playerNow
-							+ ", unfortunately you have used up all your guess. You now have to enter your answer: ");
+
+				if (userInput.equals("1")) {
+					System.out.print(playerNow + ", please type your answer: ");
 					crossed = true;
 					while (crossed) {
-						guess = keyboard.nextLine().toUpperCase();
+						guess = keyboard.nextLine().toUpperCase().trim();
 						crossed = false;
-						for (int f = 0; f < guess.length(); f++) {
-							if (ALPHANUMERALS.indexOf(message.charAt(f)) == INDEX_NEED) {
-								System.out.println(
-										playerNow + ", please enter your answer using alphanumeric characters!!");
+						for (int i = 0; i < guess.length(); i++) {
+							if (ALPHANUMERALS.indexOf(guess.charAt(i)) == INDEX_NEED) {
+								System.out.print(" You can't do that, please enter an alphanumeric character only: ");
 								guess = keyboard.nextLine().toUpperCase();
 								crossed = true;
-
+								break;
 							}
 						}
-
 					}
 
 					if (guess.equals(message)) {
-						System.out.println("CORRECT!");
+						System.out.println(playerNow + ", thats right!");
+						round = false;
 						guessed--;
+
 					} else {
-						System.out.println("WRONG!");
+						System.out.println(playerNow + ", sorry thats wrong!");
+					}
+				} else if (userInput.equals("2")) {
+					System.out.print(" Characters you didn't use yet: ");
+					for (int i = 0; i < guesses.length(); i++) {
+						System.out.print(guesses.charAt(i) + " ");
+					}
+					System.out.println(playerNow + ", please guess a single character: ");
+					crossed = true;
+					while (crossed) {
+						guess = keyboard.nextLine().toUpperCase().trim();
+						if (NO_SPACE_ALPHANUMERALS.indexOf(guess) == INDEX_NEED) {
+							System.out.print(
+									" You can't do that, please enter a alphanumeric character, WITH NO SPACES!!");
+
+						} else if (guess.length() > 1 || guess.length() < 1) {
+							System.out.print("You can't do that, please guess a letter!!");
+						} else if (guesses.indexOf(guess) == INDEX_NEED) {
+							System.out.print(
+									" You can't do that, please guess a character YOU HAVE NOT GUESSED BEFORE!!");
+						} else {
+							crossed = false;
+							guesses = guesses.substring(CHAR_BEGIN, guess.indexOf(guess)) + " _ "
+									+ guesses.substring(guess.indexOf(guess) + CHAR_OVER_ONE);
+						}
+					}
+
+					if (message.indexOf(guess) != INDEX_NEED) {
+						System.out.println(playerNow + ", you have already guessed this character!!");
+						for (int i = 0; i < cover.length(); i++) {
+							if (message.substring(i, i + CHAR_OVER_ONE).equals(guess))
+								cover = cover.substring(CHAR_BEGIN, i) + guess + cover.substring(i + CHAR_OVER_ONE);
+
+						}
+					} else {
+						System.out.println(playerNow + " this character is not in the string yet!!");
+					}
+
+					for (int i = 0; i < cover.length(); i++) {
+						System.out.print(cover.charAt(i) + " ");
+					}
+
+					System.out.println();
+					if (cover.indexOf("_") == INDEX_NEED) {
+						System.out.println("YOU WON!!!!!!!!!!!!!");
+						guessed--;
+						round = false;
 					}
 
 				}
-				
-	if (rounds % PLAYER_TURN == PLAYER_ONE_TURN)
-		playerOnePoints += POINTS - guessed;
-	if (rounds % PLAYER_TURN == PLAYER_TWO_TURN)
-		playerTwoPoints += POINTS - guessed;
-			
-	
-	System.out.println("Total player points");
-	System.out.println(player1 + ": " + playerOnePoints);
-	System.out.println(player2 + ": " + playerTwoPoints);
-	
-	if (rounds >= ROUNDS) {
-		if (playerOnePoints > playerTwoPoints){
-			System.out.println("NICE JOB!!" + player1 + "YOU HAVE WON!!!");
-			endGame = true;
-		} else if
-			(playerOnePoints < playerTwoPoints){
-				System.out.println("NICE JOB!!" + player2 + "YOU HAVE WON!!!");		
-			endGame = true; 
-		} else {
-			System.out.println(" ITS A TIE GAME!!. We are going to have a tie breaker round now");
-		}
-	} rounds++;
-	
-	keyboard.close();
-	
-		} 
-		
-	} 
-	
-} 
-	
 
+				guessed++;
+			}
+			if (guessed == GUESS) {
+				System.out.print(playerNow
+						+ ", unfortunately you have used up all your guess. You now have to enter your answer: ");
+				crossed = true;
+				while (crossed) {
+					guess = keyboard.nextLine().toUpperCase();
+					crossed = false;
+					for (int i = 0; i < guess.length(); i++) {
+						if (ALPHANUMERALS.indexOf(message.charAt(i)) == INDEX_NEED) {
+							System.out
+									.println(playerNow + ", please enter your answer using alphanumeric characters!!");
+							guess = keyboard.nextLine().toUpperCase().trim();
+							crossed = true;
+
+						}
+					}
+
+				}
+
+				if (guess.equals(message)) {
+					System.out.println("CORRECT!");
+					guessed--;
+				} else {
+					System.out.println("WRONG!");
+				}
+
+			}
+
+			if (rounds % PLAYER_TURN == PLAYER_ONE_TURN)
+				playerOnePoints += POINTS - guessed;
+			if (rounds % PLAYER_TURN == PLAYER_TWO_TURN)
+				playerTwoPoints += POINTS - guessed;
+
+			System.out.println("---Total player points---");
+			System.out.println(player1 + ": " + playerOnePoints);
+			System.out.println(player2 + ": " + playerTwoPoints);
+
+			if (rounds >= ROUNDS) {
+				if (playerOnePoints > playerTwoPoints) {
+					System.out.println("NICE JOB!!" + player1 + "YOU HAVE WON!!!");
+					endGame = true;
+				} else if (playerOnePoints < playerTwoPoints) {
+					System.out.println("NICE JOB!!" + player2 + "YOU HAVE WON!!!");
+					endGame = true;
+				} else {
+					System.out.println(" ITS A TIE GAME!!. We are going to have a tie breaker round now");
+				}
+			}
+			rounds++;
+
+			keyboard.close();
+
+		}
+
+	}
+
+}
